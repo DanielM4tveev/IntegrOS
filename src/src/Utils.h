67a896893ae682aaffd5ebf78823018f22becd80
DanielM4tveev <daniel.m4tveev@gmail.com>
2025-01-11 21:26:20 +0300

    So Many Changes

--- src/src/Utils.h
+++ src/src/Utils.h
@@ -4,8 +4,16 @@
 #include <Arduino.h>
 
 void whoami() {
+    const char* whoLines[] = {
+            "Root",
+    };
+
+    const int numLines = sizeof(whoLines) / sizeof(whoLines[0]);
+
     Serial.println("");
-    Serial.println("root");
+    for (int i = 0; i < numLines; i++) {
+        Serial.println(whoLines[i]); // Вывод каждой строки через серийный порт
+    }
     Serial.print("#!/ ");
 }
 
@@ -30,11 +38,20 @@ void uname() {
 
 
 void help() {
+    const char* helpLines[] = {
+            "help",         // Версия прошивки
+            "whoami", // Частота процессора
+            "uname", // Дата компиляции
+            "reboot"// Время компиляции
+    };
+
+    const int numLines = sizeof(helpLines) / sizeof(helpLines[0]);
+
     Serial.println("");
-    Serial.println("help");
-    Serial.println("whoami");
-    Serial.println("uname");
-    Serial.print("#!/ "); 
+    for (int i = 0; i < numLines; i++) {
+        Serial.println(helpLines[i]); // Вывод каждой строки через серийный порт
+    }
+    Serial.print("#!/ ");
 }
  
 #endif
--- src/src/modules.h
+++ src/src/modules.h
@@ -2,10 +2,23 @@
 #define modules_h
 
 #include <Arduino.h>
- 
+#include <avr/wdt.h>
+
+void reboot() {
+    Serial.print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    wdt_disable();
+    wdt_enable(WDTO_15MS);
+    while (1) {}
+}
+
 void panic() {
     Serial.println("\nKernel panic - not syncing: Fatal exception in interrupt\nCode: 00 FF FA 0F\n");
-    while(1){}
+    while(1){
+        for(int i = 5; i > 0; i--) {
+            delay(_SYS_SLEEP_TICK_ * 2);
+        }
+        reboot();
+    }
 }
 
 #endif
